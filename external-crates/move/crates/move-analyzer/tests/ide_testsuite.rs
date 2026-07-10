4b82d050ae81a3b611e889219edbda85dbdf5896
ChangZechuan <66781781@qq.com>
2026-07-11 06:33:04 +0800

    fix(move-ide): preserve string constants in hover text

--- external-crates/move/crates/move-analyzer/tests/ide_testsuite.rs
+++ external-crates/move/crates/move-analyzer/tests/ide_testsuite.rs
@@ -573,7 +573,7 @@ fn use_def_test_rtdte<F: MoveFlavor>(
             // We do incremental compilation only for the first file in the test rtdte.
             // The results for remaining files should still be correct due to all symbols
             // being computed during the initial full compilation at rtdte level, and
-            // due to merging of symbols from modified and unmofdified files
+            // due to merging of symbols from modified and unmodified files
             // (which is what it is being tested here).
 
             let original = std::fs::read_to_string(&cpath)?;
